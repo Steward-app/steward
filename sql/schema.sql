73509e0293d2bdf62ec1171384fb2ea0020d3d85
Artanicus <artanicus@nocturnal.fi>
2018-09-14 13:33:17 +0300

    Improve schema by implementing snoozes and separating schedules

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,36 @@
-PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE calendars (id integer PRIMARY KEY, name text NOT NULL, calendar_id text NOT NULL, description text);
-CREATE TABLE assets (id integer PRIMARY KEY, name text NOT NULL, description text, schedule_id integer, calendar_id integer, FOREIGN KEY (calendar_id) REFERENCES calendars (id), FOREIGN KEY (schedule_id) REFERENCES schedules (id));
+CREATE TABLE `snoozes` (
+	`id`	INTEGER NOT NULL,
+	`name`	TEXT NOT NULL,
+	`length`	TEXT NOT NULL,
+	PRIMARY KEY(id)
+);
 CREATE TABLE schedules (id integer PRIMARY KEY, name text NOT NULL, description text, recurrence text NOT NULL);
+CREATE TABLE "maintenances" (
+	`id`	INTEGER NOT NULL,
+	`name`	TEXT NOT NULL,
+	`description`	TEXT,
+	`schedule`	INTEGER,
+	`snooze`	INTEGER NOT NULL,
+	`asset`	INTEGER NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(`schedule`) REFERENCES schedules ( id ),
+	FOREIGN KEY(`snooze`) REFERENCES snoozes ( id ),
+	FOREIGN KEY(`asset`) REFERENCES assets(id)
+);
+CREATE TABLE "calendars" (
+	`id`	integer,
+	`name`	text NOT NULL,
+	`api_id`	text NOT NULL,
+	`description`	text,
+	PRIMARY KEY(id)
+);
+CREATE TABLE "assets" (
+	`id`	integer,
+	`name`	text NOT NULL,
+	`description`	text,
+	`calendar_id`	integer,
+	PRIMARY KEY(id),
+	FOREIGN KEY(`calendar_id`) REFERENCES calendars ( id )
+);
 COMMIT;
