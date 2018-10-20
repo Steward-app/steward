79dd3fb60f10afd473ad94a17507145fcc0c9701
Artanicus <artanicus@nocturnal.fi>
2018-10-20 14:57:20 +0300

    Make sure not nulls are everywhere

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,31 +5,37 @@ CREATE TABLE `snoozes` (
 	`length`	TEXT NOT NULL,
 	PRIMARY KEY(id)
 );
-CREATE TABLE schedules (id integer PRIMARY KEY, name text NOT NULL, description text, recurrence text NOT NULL);
+CREATE TABLE "schedules" (
+	`id`	INTEGER NOT NULL,
+	`name`	text NOT NULL,
+	`description`	text,
+	`recurrence`	text NOT NULL,
+	PRIMARY KEY(id)
+);
 CREATE TABLE "maintenances" (
 	`id`	INTEGER NOT NULL,
 	`name`	TEXT NOT NULL,
 	`description`	TEXT,
-	`schedule`	INTEGER,
-	`snooze`	INTEGER NOT NULL,
+	`schedule`	INTEGER NOT NULL,
+	`snooze`	TEXT NOT NULL,
 	`asset`	INTEGER NOT NULL,
 	PRIMARY KEY(id),
 	FOREIGN KEY(`schedule`) REFERENCES schedules ( id ),
 	FOREIGN KEY(`snooze`) REFERENCES snoozes ( id ),
-	FOREIGN KEY(`asset`) REFERENCES assets(id)
+	FOREIGN KEY(`asset`) REFERENCES assets ( id )
 );
 CREATE TABLE "calendars" (
-	`id`	integer,
+	`id`	INTEGER NOT NULL,
 	`name`	text NOT NULL,
 	`api_id`	text NOT NULL,
 	`description`	text,
 	PRIMARY KEY(id)
 );
 CREATE TABLE "assets" (
-	`id`	integer,
+	`id`	INTEGER NOT NULL,
 	`name`	text NOT NULL,
 	`description`	text,
-	`calendar_id`	integer,
+	`calendar_id`	integer NOT NULL,
 	PRIMARY KEY(id),
 	FOREIGN KEY(`calendar_id`) REFERENCES calendars ( id )
 );
