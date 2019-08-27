9e13ebc89ab4c01252e9e750176ebfa1b4340868
Artanicus <artanicus@nocturnal.fi>
2019-08-27 22:41:41 +0300

    Pull user data from backend live

--- web/__init__.py
+++ web/__init__.py
@@ -3,21 +3,25 @@
 __version__ = "0.0.1"
 
 from absl import logging
-
 from flask import Flask, render_template
+import grpc
+
+from steward import user_pb2 as u
+from steward import registry_pb2 as r
+from steward import registry_pb2_grpc
+
+
 app = Flask(__name__)
 app.config.from_object('websiteconfig')
 
-users = {
-        42: 'pertti',
-        666: 'luci'
-        }
+channel = grpc.insecure_channel('localhost:50051')
+users = registry_pb2_grpc.UserServiceStub(channel)
 
 @app.route('/')
 def root():
-    return render_template('index.html', users=users)
+    return render_template('index.html', users=users.ListUsers(u.ListUsersRequest()))
 
 @app.route('/user/<id>')
 def user(id=None):
-    return render_template('user.html', user=users[int(id)])
-    
+    return render_template('user.html', user=users.GetUser(u.User(_id=id)))
+
