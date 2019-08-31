f18d341fa97021b89169cd21f0b5f83c4495f0f8
Artanicus <artanicus@nocturnal.fi>
2019-08-31 19:53:06 +0300

    Separate storage layer to separate module

---
+++ registry/storage.py
@@ -0,0 +1,34 @@
+from absl import logging
+
+import pymongo
+from google.protobuf.json_format import MessageToDict, ParseDict
+from bson.objectid import ObjectId
+
+from steward import user_pb2 as u
+
+class StorageManager():
+    def __init__(self, env):
+        self.mongo_client = pymongo.MongoClient('mongodb://steward.lxc:27017')
+        self.db = self.mongo_client['steward-'+env]
+        self.users = self.db.user
+
+    def encode(self, proto):
+        return self._proto2dict(proto)
+
+    def decode(self, bson, message):
+        return self._dict2proto(bson, message)
+
+    def _proto2dict(self, proto):
+        logging.warning("proto before conversion: {}".format(proto))
+        dict_out = MessageToDict(proto)
+        logging.warning("bson dict after conversion: {}".format(dict_out))
+        return dict_out
+
+    def _dict2proto(self, bson, message):
+        logging.warning("bson dict before conversion: {}".format(bson))
+        bson['_id'] = str(bson['_id']) # ObjectId -> str _id
+        logging.warning("bson dict after field conversions: {}".format(bson))
+        proto = message()
+        ParseDict(bson, proto)
+        logging.warning("proto after conversion: {}".format(proto))
+        return proto
--- registry/user_server.py
+++ registry/user_server.py
@@ -3,12 +3,11 @@ from concurrent import futures
 from absl import logging
 
 import grpc
-import pymongo
-from google.protobuf.json_format import MessageToDict, ParseDict
-from google.protobuf import text_format
 
 from bson.objectid import ObjectId
 from bson.json_util import dumps, loads
+
+from registry import storage
 
 from steward import user_pb2 as u
 from steward import registry_pb2 as r
@@ -20,53 +19,30 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class UserServiceServicer(registry_pb2_grpc.UserServiceServicer):
     def __init__(self, env):
-        self.mongo_client = pymongo.MongoClient('mongodb://steward.lxc:27017')
-        self.db = self.mongo_client['steward-'+env]
-        self.users = self.db.user
+        self.storage = storage.StorageManager(env=env)
 
     def GetUser(self, request, context):
         id = request._id
         if not isinstance(id, ObjectId):
             id = ObjectId(id)
         print('GetUser id type: {}'.format(type(id)))
-        data_bson = self.users.find_one({'_id': id})
-        return self.decode(data_bson, u.User)
+        data_bson = self.storage.users.find_one({'_id': id})
+        return self.storage.decode(data_bson, u.User)
 
     def CreateUser(self, request, context):
         user = u.User(name=request.name, email=request.email, password=request.password, available_effort=request.available_effort)
-        result = self.users.insert_one(self.encode(user))
+        result = self.storage.users.insert_one(self.storage.encode(user))
         return self.GetUser(u.GetUserRequest(_id=str(result.inserted_id)), None)
 
     def ListUsers(self, request, context):
-        for user_bson in self.users.find():
+        for user_bson in self.storage.users.find():
             user = u.User()
-            self.decode(user_bson)
+            self.storage.decode(user_bson)
             yield user
 
-    def encode(self, proto):
-        return self._proto2dict(proto)
-
-    def decode(self, bson, message):
-        return self._dict2proto(bson, message)
-
-    def _proto2dict(self, proto):
-        logging.warning("proto before conversion: {}".format(proto))
-        dict_out = MessageToDict(proto)
-        logging.warning("bson dict after conversion: {}".format(dict_out))
-        return dict_out
-
-    def _dict2proto(self, bson, message):
-        logging.warning("bson dict before conversion: {}".format(bson))
-        bson['_id'] = str(bson['_id']) # ObjectId -> str _id
-        logging.warning("bson dict after field conversions: {}".format(bson))
-        proto = message()
-        ParseDict(bson, proto)
-        logging.warning("proto after conversion: {}".format(proto))
-        return proto
-
-def serve():
+def serve(env):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    registry_pb2_grpc.add_UserServiceServicer_to_server(UserServiceServicer(env='dev'), server)
+    registry_pb2_grpc.add_UserServiceServicer_to_server(UserServiceServicer(env=env), server)
     server.add_insecure_port('[::]:50051')
     server.start()
 
@@ -77,4 +53,4 @@ def serve():
         server.stop(0)
 
 if __name__ == '__main__':
-    serve()
+    serve(env='dev')
