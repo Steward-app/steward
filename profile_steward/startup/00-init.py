5871e007bb78185abac6389230abd2e778c5c832
Artanicus <artanicus@nocturnal.fi>
2019-10-26 20:33:05 +0300

    Enable storage debugging in the ipython profile

--- profile_steward/startup/00-init.py
+++ profile_steward/startup/00-init.py
@@ -1,8 +1,19 @@
-import grpc
+import grpc, os
+from absl import logging, flags
 from steward import user_pb2 as u
 from steward import maintenance_pb2 as m
 from steward import registry_pb2_grpc
 
+FLAGS = flags.FLAGS
+
+from registry import storage
+
+if 'STEWARD_DB' in os.environ:
+    FLAGS.db = os.environ['STEWARD_DB']
+
 channel = grpc.insecure_channel('localhost:50051')
 users = registry_pb2_grpc.UserServiceStub(channel)
 maintenances = registry_pb2_grpc.MaintenanceServiceStub(channel)
+
+flags.FLAGS.mark_as_parsed()
+s = storage.StorageManager()
