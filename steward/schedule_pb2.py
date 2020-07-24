298ee16630b35f5fbf58caf1a19d2ba81d572514
Artanicus <artanicus@nocturnal.fi>
2020-07-24 16:47:41 +0300

    internalize Duration

--- steward/schedule_pb2.py
+++ steward/schedule_pb2.py
@@ -12,7 +12,6 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-from google.protobuf import duration_pb2 as google_dot_protobuf_dot_duration__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
@@ -21,9 +20,8 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x16steward/schedule.proto\x12\x07steward\x1a\x1egoogle/protobuf/duration.proto\"\xcb\x01\n\x08Schedule\x12\x0b\n\x03_id\x18\x01 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x02 \x01(\t\x12#\n\x04type\x18\x03 \x01(\x0e\x32\x15.steward.ScheduleType\x12+\n\x08interval\x18\x04 \x01(\x0b\x32\x19.google.protobuf.Duration\x12\x1f\n\x06snooze\x18\x05 \x01(\x0b\x32\x0f.steward.Snooze\x12*\n\x11\x61vailable_snoozes\x18\x06 \x03(\x0b\x32\x0f.steward.Snooze\"J\n\x06Snooze\x12\x13\n\x0b\x64\x65scription\x18\x01 \x01(\t\x12+\n\x08\x64uration\x18\x02 \x01(\x0b\x32\x19.google.protobuf.Duration**\n\x0cScheduleType\x12\x0c\n\x08\x44URATION\x10\x00\x12\x0c\n\x08\x45XTERNAL\x10\x01\x62\x06proto3'
-  ,
-  dependencies=[google_dot_protobuf_dot_duration__pb2.DESCRIPTOR,])
+  serialized_pb=b'\n\x16steward/schedule.proto\x12\x07steward\"\xc3\x01\n\x08Schedule\x12\x0b\n\x03_id\x18\x01 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x02 \x01(\t\x12#\n\x04type\x18\x03 \x01(\x0e\x32\x15.steward.ScheduleType\x12#\n\x08interval\x18\x04 \x01(\x0b\x32\x11.steward.Duration\x12\x1f\n\x06snooze\x18\x05 \x01(\x0b\x32\x0f.steward.Snooze\x12*\n\x11\x61vailable_snoozes\x18\x06 \x03(\x0b\x32\x0f.steward.Snooze\"B\n\x06Snooze\x12\x13\n\x0b\x64\x65scription\x18\x01 \x01(\t\x12#\n\x08\x64uration\x18\x02 \x01(\x0b\x32\x11.steward.Duration\"\x1b\n\x08\x44uration\x12\x0f\n\x07seconds\x18\x01 \x01(\r**\n\x0cScheduleType\x12\x0c\n\x08\x44URATION\x10\x00\x12\x0c\n\x08\x45XTERNAL\x10\x01\x62\x06proto3'
+)
 
 _SCHEDULETYPE = _descriptor.EnumDescriptor(
   name='ScheduleType',
@@ -45,8 +43,8 @@ _SCHEDULETYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=349,
-  serialized_end=391,
+  serialized_start=330,
+  serialized_end=372,
 )
 _sym_db.RegisterEnumDescriptor(_SCHEDULETYPE)
 
@@ -118,8 +116,8 @@ _SCHEDULE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=68,
-  serialized_end=271,
+  serialized_start=36,
+  serialized_end=231,
 )
 
 
@@ -157,17 +155,50 @@ _SNOOZE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=273,
-  serialized_end=347,
+  serialized_start=233,
+  serialized_end=299,
+)
+
+
+_DURATION = _descriptor.Descriptor(
+  name='Duration',
+  full_name='steward.Duration',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='seconds', full_name='steward.Duration.seconds', index=0,
+      number=1, type=13, cpp_type=3, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=301,
+  serialized_end=328,
 )
 
 _SCHEDULE.fields_by_name['type'].enum_type = _SCHEDULETYPE
-_SCHEDULE.fields_by_name['interval'].message_type = google_dot_protobuf_dot_duration__pb2._DURATION
+_SCHEDULE.fields_by_name['interval'].message_type = _DURATION
 _SCHEDULE.fields_by_name['snooze'].message_type = _SNOOZE
 _SCHEDULE.fields_by_name['available_snoozes'].message_type = _SNOOZE
-_SNOOZE.fields_by_name['duration'].message_type = google_dot_protobuf_dot_duration__pb2._DURATION
+_SNOOZE.fields_by_name['duration'].message_type = _DURATION
 DESCRIPTOR.message_types_by_name['Schedule'] = _SCHEDULE
 DESCRIPTOR.message_types_by_name['Snooze'] = _SNOOZE
+DESCRIPTOR.message_types_by_name['Duration'] = _DURATION
 DESCRIPTOR.enum_types_by_name['ScheduleType'] = _SCHEDULETYPE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -184,6 +215,13 @@ Snooze = _reflection.GeneratedProtocolMessageType('Snooze', (_message.Message,),
   # @@protoc_insertion_point(class_scope:steward.Snooze)
   })
 _sym_db.RegisterMessage(Snooze)
+
+Duration = _reflection.GeneratedProtocolMessageType('Duration', (_message.Message,), {
+  'DESCRIPTOR' : _DURATION,
+  '__module__' : 'steward.schedule_pb2'
+  # @@protoc_insertion_point(class_scope:steward.Duration)
+  })
+_sym_db.RegisterMessage(Duration)
 
 
 # @@protoc_insertion_point(module_scope)
