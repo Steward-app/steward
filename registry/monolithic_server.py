11bb8e2b561588d9604461c12fff82f916ac3a92
Artanicus <artanicus@nocturnal.fi>
2020-07-26 09:28:55 +0300

    Serve the asset server from the monolithic

--- registry/monolithic_server.py
+++ registry/monolithic_server.py
@@ -6,8 +6,9 @@ from grpc_reflection.v1alpha import reflection
 
 from steward import user_pb2 as u
 from steward import maintenance_pb2 as m
+from steward import asset_pb2 as a
 from steward import registry_pb2_grpc, registry_pb2
-from registry import server_flags, user_server, maintenance_server, storage
+from registry import server_flags, user_server, maintenance_server, asset_server, storage
 
 FLAGS = flags.FLAGS
 
@@ -18,9 +19,11 @@ def serve(argv):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     registry_pb2_grpc.add_UserServiceServicer_to_server(user_server.UserServiceServicer(storage_manager=s), server)
     registry_pb2_grpc.add_MaintenanceServiceServicer_to_server(maintenance_server.MaintenanceServiceServicer(storage_manager=s), server)
+    registry_pb2_grpc.add_AssetServiceServicer_to_server(asset_server.AssetServiceServicer(storage_manager=a), server)
     SERVICE_NAMES = (
             registry_pb2.DESCRIPTOR.services_by_name['UserService'].full_name,
             registry_pb2.DESCRIPTOR.services_by_name['MaintenanceService'].full_name,
+            registry_pb2.DESCRIPTOR.services_by_name['AssetService'].full_name,
             reflection.SERVICE_NAME,
             )
     reflection.enable_server_reflection(SERVICE_NAMES, server)
